d87a11d6284ef366aa97b6666896751c37e168b0
Lucas Souza <lucassouza@MacBook-Air-de-Lucas.local>
2023-02-08 12:50:18 -0400

    fix(auth): public login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 
@@ -15,6 +16,7 @@ import { AuthRequest } from './models/AuthRequest';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
