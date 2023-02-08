cdfe77f7c72ca6f522c2d4fb0d17f08415ef04f1
Lucas Souza <lucassouza@MacBook-Air-de-Lucas.local>
2023-02-08 11:41:57 -0400

    feat(auth): implement local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private readonly userService: UserService) {}
 
   login() {}
 
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) return { ...user, password: undefined };
+    }
+
+    throw new Error('Invalid email or password');
   }
 }
