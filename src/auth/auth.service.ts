bc066223aca99fcb32846612f58d4845f38e17cd
Lucas Souza <lucassouza@MacBook-Air-de-Lucas.local>
2023-02-08 11:18:51 -0400

    feat(auth): implement local strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  login() {}
+
+  validateUser(email: string, password: string) {
+    throw new Error('Method not implemented.');
+  }
+}
